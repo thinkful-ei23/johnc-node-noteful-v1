edc188c458b95f220f36736f67ec97775fa998b2
john <johnjacobcosgrove@gmail.com>
2018-07-30 15:19:13 -0400

    added express static

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 'use strict';
 
+
+
 // Load array of notes
 const data = require('./db/notes');
 
 console.log('Hello Noteful!');
 
 // INSERT EXPRESS APP CODE HERE...
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.listen(8080, function () {
+    console.info(`Server listening on ${this.address().port}`);
+  }).on('error', err => {
+    console.error(err);
+  });
+
