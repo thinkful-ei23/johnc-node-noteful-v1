f2a676ee7f66adecbadef19a6011d77d3d4c2e97
john <johnjacobcosgrove@gmail.com>
2018-07-31 22:14:37 -0400

    updated /api/notes

--- server.js
+++ server.js
@@ -4,7 +4,9 @@
 
 // Load array of notes
 const data = require('./db/notes');
-const {PORT} = require('./config')
+const {PORT} = require('./config');
+const simDB = require('./db/simDB'); 
+const notes = simDB.initialize(data);
 
 console.log('Hello Noteful!');
 
@@ -14,20 +16,18 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(PORT, function () {
-    console.info(`Server listening on ${this.address().port}`);
-  }).on('error', err => {
-    console.error(err);
-  });
 
-app.get('/api/notes/',(req,res)=>{
-    const queryObject = req.query
-    let list = data
-    if(queryObject.searchTerm){
-        list = list.filter(item => item.title === queryObject.searchTerm)
+
+app.get('/api/notes', (req, res, next) => {
+  const { searchTerm } = req.query;
+
+  notes.filter(searchTerm, (err, list) => {
+    if (err) {
+      return next(err);
     }
     res.json(list);
-})
+  });
+});
 
 app.get('/api/notes/:id', (req,res)=>{
     const id = req.params.id;
@@ -35,3 +35,22 @@ app.get('/api/notes/:id', (req,res)=>{
     res.json(item);
 })
 
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    res.status(404).json({ message: 'Not Found' });
+  });
+
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  });
+
+app.listen(PORT, function () {
+    console.info(`Server listening on ${this.address().port}`);
+  }).on('error', err => {
+    console.error(err);
+  });
